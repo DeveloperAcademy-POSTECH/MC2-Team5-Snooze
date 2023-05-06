b6ea6c8c4ad82aa40ad738b7bb457d690e37a574
sunghun <leedool3003@gmail.com>
2023-05-06 18:29:47 +0900

    :hammer: [chore] AnimalType, Profile Entity Set #4

---
+++ OnJeWa/Domain/Entities/Profile.swift
@@ -0,0 +1,18 @@
+//
+//  Profile.swift
+//  OnJeWa
+//
+//  Created by Kim SungHun on 2023/05/05.
+//
+
+import UIKit
+import CoreLocation
+
+struct Profile {
+    var animalType: String?
+    var profileImage: UIImage?
+    var name: String?
+    var birthDate: Date?
+    var backgroundImage: UIImage?
+    var coordinate: CLLocationCoordinate2D?
+}
