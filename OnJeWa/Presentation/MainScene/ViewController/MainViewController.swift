a934b8403293f14e18f93ce49bbec66e0b3812b4
sunghun <leedool3003@gmail.com>
2023-05-15 04:14:33 +0900

    :sparkles: [feat] 위치 기반 알림 추가

--- OnJeWa/Presentation/MainScene/ViewController/MainViewController.swift
+++ OnJeWa/Presentation/MainScene/ViewController/MainViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 import OnJeWaCore
 import OnJeWaUI
+import OnJeWaNetwork
+import CoreLocation
 
-class MainViewController: BaseViewController {
+class MainViewController: BaseViewController, CLLocationManagerDelegate {
     
     //MARK: - Properties
     
     let viewModel = MainViewModel()
+    let locationManager = CLLocationManager()
     
     //MARK: - View Components
     
@@ -26,9 +29,72 @@ class MainViewController: BaseViewController {
     //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        UNUserNotificationCenter.current().requestAuthorization(
+             options: [.alert, .badge, .sound]
+         ) {
+             success, error in
+         }
+         
+         locationManager.requestAlwaysAuthorization()
+         locationManager.allowsBackgroundLocationUpdates = true
+         
+         // 위치 서비스 정확도 설정
+         locationManager.desiredAccuracy = kCLLocationAccuracyBest
+         locationManager.delegate = self
+         
+         // 위치 서비스 업데이트 시작
+         locationManager.startUpdatingLocation()
+        
+        
         tapButtonVC.delegate = self // 내가 대신 하겠다
         navigationView.delegate = self
         weekCollectionView.delegate = self
+        
+        print("test 1 \(RealmManager.shared.readCoordinate().coordinate.latitude)")
+        print("test 2 \(RealmManager.shared.readCoordinate().coordinate.longitude)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            // 범위를 설정하는 기준좌표 -> 예를 들어 우리 앱에서는 집 혹은 회사
+            
+            let center = CLLocationCoordinate2D(latitude: RealmManager.shared.readCoordinate().coordinate.latitude, longitude: RealmManager.shared.readCoordinate().coordinate.longitude)
+            
+            // 200m의 원형 영역
+            let region = CLCircularRegion(center: center, radius: 200.0, identifier: "Geofence")
+            
+            region.notifyOnEntry = true
+            region.notifyOnExit = true
+            
+            locationManager.startMonitoring(for: region)
+        }
+    }
+    
+    // 지정한 지역에 들어왔을 때 수행할 작업
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        print("사용자가 지정한 지역에 들어왔습니다.")
+        
+        let content = UNMutableNotificationContent()
+        content.title = "집에 들어오셨군요!"
+        content.body = "들어오기를 눌러주세요!"
+        content.sound = .default
+        
+        let request = UNNotificationRequest(identifier: "Geofence", content: content, trigger: nil)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
+    
+    // 지정한 지역을 벗어났을 때 수행할 작업
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        print("사용자가 지정한 지역을 벗어났습니다.")
+        
+        let content = UNMutableNotificationContent()
+        content.title = "집을 나오셨군요!"
+        content.body = "외출하기를 눌러주세요!"
+        content.sound = .default
+        
+        let request = UNNotificationRequest(identifier: "Geofence", content: content, trigger: nil)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
     }
     
     override func viewWillAppear(_ animated: Bool) {
